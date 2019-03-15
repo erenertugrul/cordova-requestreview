c4b95004609a204a43a291bc7a16b9e22ae3cc75
efe <efe@EFES-macbookair.local>
2019-03-15 09:51:56 +0300

    refactoring

--- src/ios/RequestReview.swift
+++ src/ios/RequestReview.swift
@@ -1,87 +1,71 @@
 import StoreKit
 
-@objc(RequestReview) class RequestReview : CDVPlugin {
-  @objc(requestReview:)
-  func requestReview(command: CDVInvokedUrlCommand) {
-        func basarili(yazi: String)
-        {
-            let sonuc = CDVPluginResult(
-                status: CDVCommandStatus_OK,
-                messageAs: yazi
-            )
-            self.commandDelegate.send(
-                sonuc,
-                callbackId: command.callbackId
-            )
-        }
+@objc(RequestReview) class RequestReview : CDVPlugin
+{
+    private func basarili(yazi: String)
+    {
+        let sonuc = CDVPluginResult(
+            status: CDVCommandStatus_OK,
+            messageAs: yazi
+        )
+        self.commandDelegate.send(
+            sonuc,
+            callbackId: command.callbackId
+        )
+    }
     
-        func hatali(yazi: String)
-        {
-            let sonuc = CDVPluginResult(
-                status: CDVCommandStatus_ERROR,
-                messageAs: yazi
-            )
-            self.commandDelegate.send(
-                sonuc,
-                callbackId: command.callbackId
-            )
-        }
+    private func hatali(yazi: String)
+    {
+        let sonuc = CDVPluginResult(
+            status: CDVCommandStatus_ERROR,
+            messageAs: yazi
+        )
+        self.commandDelegate.send(
+            sonuc,
+            callbackId: command.callbackId
+        )
+    }
+    
+    @objc(requestReview:)
+    func requestReview(command: CDVInvokedUrlCommand)
+    {
         if #available(iOS 10.3, *) {
             SKStoreReviewController.requestReview()
-            basarili(yazi: "gösterildi")
+            self.basarili(yazi: "gösterildi")
         }
         else{
-            hatali(yazi: "ios sürümü 10.3 den yüksek olmalı")
+            self.hatali(yazi: "ios sürümü 10.3 den yüksek olmalı")
         }
     }
+    
+    
     @objc(writeReview:)
-    func writeReview(command: CDVInvokedUrlCommand) {
-        func basarili(yazi: String)
-        {
-            let sonuc = CDVPluginResult(
-                status: CDVCommandStatus_OK,
-                messageAs: yazi
-            )
-            self.commandDelegate.send(
-                sonuc,
-                callbackId: command.callbackId
-            )
-        }
-        
-        func hatali(yazi: String)
-        {
-            let sonuc = CDVPluginResult(
-                status: CDVCommandStatus_ERROR,
-                messageAs: yazi
-            )
-            self.commandDelegate.send(
-                sonuc,
-                callbackId: command.callbackId
-            )
-        }
-        
+    func writeReview(command: CDVInvokedUrlCommand)
+    {
         let uygulama_id = command.arguments[0] as? String ?? ""
         if (uygulama_id == ""){
             hatali(yazi: "uygulama id'si yok")
             return
         }
-
+        
         if #available(iOS 10.3, *) {
             
             if let url = URL(string: "https://itunes.apple.com/us/app/"+uygulama_id+"?action=write-review"){
                 UIApplication.shared.open(url,options: [:],completionHandler: {(result) in
                     if result{
-                        basarili(yazi: "site açıldı")
+                        self.basarili(yazi: "site açıldı")
                     }
                     else{
-                        hatali(yazi: "site açılmadı")
+                        self.hatali(yazi: "site açılmadı")
                     }
                 })
             }
         }
         else{
-            hatali(yazi: "ios sürümü 10.3 den yüksek olmalı")
+            self.hatali(yazi: "ios sürümü 10.3 den yüksek olmalı")
         }
-
+        
     }
+
+    
 }
